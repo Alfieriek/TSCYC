e7627ca41da3d55611db9f06c6a29fc360e628ba
Alfieriek <alfieriek@gmail.com>
2024-03-29 22:43:06 -0500

    T scores for males and females

---
+++ RawScores2TScores.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(tidyr)
+library(readxl)
+
+
+Jan21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 1) 
+May21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 2) 
+Dec21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 3) 
+May22_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 4) 
