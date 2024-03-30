e2cfa4bc50182662739a4e3abb5c2ca8be818ff3
Alfieriek <alfieriek@gmail.com>
2024-03-30 01:58:44 -0500

    Fixed PST1 issue

--- Female_RawScores2TScores.R
+++ Female_RawScores2TScores.R
@@ -4,12 +4,12 @@ library(readxl)
 library(tidyverse)
 
 
-# May22_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 1)
-# May22_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 2)
-# May22_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 3)
-May22_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 4)
+# Dec21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 1)
+# Dec21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 2)
+Dec21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 3)
+# Dec21_RS <- read_excel("TSCYC_clean.3.29.xlsx",sheet = 4)
 
-May22_F_RS <- May22_RS %>% filter(Gender == 'F')
+Dec21_F_RS <- Dec21_RS %>% filter(Gender == 'F')
 
 
 #_________________________________________________________1
@@ -17,10 +17,10 @@ May22_F_RS <- May22_RS %>% filter(Gender == 'F')
 ########################### RL #########################
 Female_Rl_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 1)
 
-A1 <- May22_F_RS %>% select(ID,May22_RL_RawScore) %>% rename(Raw_Score = May22_RL_RawScore)
+A1 <- Dec21_F_RS %>% select(ID,Dec21_RL_RawScore) %>% rename(Raw_Score = Dec21_RL_RawScore)
 
-May22_F_Rl_TS <- merge(A1, Female_Rl_TS, by = "Raw_Score", all = FALSE )
-May22_F_Rl_TS <- May22_F_Rl_TS %>% rename(RL_Raw_Score = Raw_Score,
+Dec21_F_Rl_TS <- merge(A1, Female_Rl_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_Rl_TS <- Dec21_F_Rl_TS %>% rename(RL_Raw_Score = Raw_Score,
                                           RL_T = T,
                                           RL_Ptile  = Percentile)
 
@@ -31,10 +31,10 @@ May22_F_Rl_TS <- May22_F_Rl_TS %>% rename(RL_Raw_Score = Raw_Score,
 ########################### ATR #########################
 Female_ATR_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 2)
 
-B1 <- May22_F_RS %>% select(ID,May22_ATR_RawScore) %>% rename(Raw_Score = May22_ATR_RawScore)
+B1 <- Dec21_F_RS %>% select(ID,Dec21_ATR_RawScore) %>% rename(Raw_Score = Dec21_ATR_RawScore)
 
-May22_F_ATR_TS <- merge(B1, Female_ATR_TS, by = "Raw_Score", all = FALSE )
-May22_F_ATR_TS <- May22_F_ATR_TS %>% rename(ATR_Raw_Score = Raw_Score,
+Dec21_F_ATR_TS <- merge(B1, Female_ATR_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_ATR_TS <- Dec21_F_ATR_TS %>% rename(ATR_Raw_Score = Raw_Score,
                                             ATR_T = T,
                                             ATR_Ptile  = Percentile)
 
@@ -45,10 +45,10 @@ May22_F_ATR_TS <- May22_F_ATR_TS %>% rename(ATR_Raw_Score = Raw_Score,
 ########################### ANX #########################
 Female_ANX_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 3)
 
-C1 <- May22_F_RS %>% select(ID,May22_ANX_RawScore) %>% rename(Raw_Score = May22_ANX_RawScore)
+C1 <- Dec21_F_RS %>% select(ID,Dec21_ANX_RawScore) %>% rename(Raw_Score = Dec21_ANX_RawScore)
 
-May22_F_ANX_TS <- merge(C1, Female_ANX_TS, by = "Raw_Score", all = FALSE )
-May22_F_ANX_TS <- May22_F_ANX_TS  %>% rename(ANX_Raw_Score = Raw_Score,
+Dec21_F_ANX_TS <- merge(C1, Female_ANX_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_ANX_TS <- Dec21_F_ANX_TS  %>% rename(ANX_Raw_Score = Raw_Score,
                                              ANX_T = T,
                                              ANX_Ptile  = Percentile)
 
@@ -59,10 +59,10 @@ May22_F_ANX_TS <- May22_F_ANX_TS  %>% rename(ANX_Raw_Score = Raw_Score,
 ########################### DEP #########################
 Female_DEP_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 4)
 
-D1 <- May22_F_RS %>% select(ID,May22_DEP_RawScore) %>% rename(Raw_Score = May22_DEP_RawScore)
+D1 <- Dec21_F_RS %>% select(ID,Dec21_DEP_RawScore) %>% rename(Raw_Score = Dec21_DEP_RawScore)
 
-May22_F_DEP_TS <- merge(D1, Female_DEP_TS, by = "Raw_Score", all = FALSE )
-May22_F_DEP_TS <- May22_F_DEP_TS  %>% rename(DEP_Raw_Score = Raw_Score,
+Dec21_F_DEP_TS <- merge(D1, Female_DEP_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_DEP_TS <- Dec21_F_DEP_TS  %>% rename(DEP_Raw_Score = Raw_Score,
                                              DEP_T = T,
                                              DEP_Ptile  = Percentile)
 
@@ -72,10 +72,10 @@ May22_F_DEP_TS <- May22_F_DEP_TS  %>% rename(DEP_Raw_Score = Raw_Score,
 ########################### ANG #########################
 Female_ANG_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 5)
 
-E1 <- May22_F_RS %>% select(ID,May22_ANG_RawScore) %>% rename(Raw_Score = May22_ANG_RawScore)
+E1 <- Dec21_F_RS %>% select(ID,Dec21_ANG_RawScore) %>% rename(Raw_Score = Dec21_ANG_RawScore)
 
-May22_F_ANG_TS <- merge(E1, Female_ANG_TS, by = "Raw_Score", all = FALSE )
-May22_F_ANG_TS <- May22_F_ANG_TS  %>% rename(ANG_Raw_Score = Raw_Score,
+Dec21_F_ANG_TS <- merge(E1, Female_ANG_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_ANG_TS <- Dec21_F_ANG_TS  %>% rename(ANG_Raw_Score = Raw_Score,
                                              ANG_T = T,
                                              ANG_Ptile  = Percentile)
 
@@ -84,11 +84,11 @@ May22_F_ANG_TS <- May22_F_ANG_TS  %>% rename(ANG_Raw_Score = Raw_Score,
 ########################### PTSI #########################
 Female_PTSI_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 6)
 
-f1 <- May22_F_RS %>% select(ID,May22_PTSI_RawScore) %>% rename(Raw_Score = May22_PTSI_RawScore)
+f1 <- Dec21_F_RS %>% select(ID,Dec21_PTSI_RawScore) %>% rename(Raw_Score = Dec21_PTSI_RawScore)
 
 
-May22_F_PTSI_TS <- merge(f1, Female_PTSI_TS, by = "Raw_Score", all = FALSE )
-May22_F_PTSI_TS <- May22_F_PTSI_TS %>% rename(PTSI_Raw_Score = Raw_Score,
+Dec21_F_PTSI_TS <- merge(f1, Female_PTSI_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_PTSI_TS <- Dec21_F_PTSI_TS %>% rename(PTSI_Raw_Score = Raw_Score,
                                               PTSI_T = T,
                                               PTSI_Ptile  = Percentile)
 
@@ -97,11 +97,11 @@ May22_F_PTSI_TS <- May22_F_PTSI_TS %>% rename(PTSI_Raw_Score = Raw_Score,
 ########################### PTSAV #########################
 Female_PTSAV_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 7)
 
-G1 <- May22_F_RS %>% select(ID,May22_PTSAV_RawScore) %>% rename(Raw_Score = May22_PTSAV_RawScore)
+G1 <- Dec21_F_RS %>% select(ID,Dec21_PTSAV_RawScore) %>% rename(Raw_Score = Dec21_PTSAV_RawScore)
 
 
-May22_F_PTSAV_TS <- merge(G1, Female_PTSAV_TS, by = "Raw_Score", all = FALSE )
-May22_F_PTSAV_TS <- May22_F_PTSAV_TS %>% rename(PTSAV_Raw_Score = Raw_Score,
+Dec21_F_PTSAV_TS <- merge(G1, Female_PTSAV_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_PTSAV_TS <- Dec21_F_PTSAV_TS %>% rename(PTSAV_Raw_Score = Raw_Score,
                                                 PTSAV_T = T,
                                                 PTSAV_Ptile  = Percentile)
 
@@ -111,10 +111,10 @@ May22_F_PTSAV_TS <- May22_F_PTSAV_TS %>% rename(PTSAV_Raw_Score = Raw_Score,
 ########################### PTSAR #########################
 Female_PTSAR_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 8)
 
-H1 <- May22_F_RS %>% select(ID,May22_PTSAR_RawScore) %>% rename(Raw_Score = May22_PTSAR_RawScore)
+H1 <- Dec21_F_RS %>% select(ID,Dec21_PTSAR_RawScore) %>% rename(Raw_Score = Dec21_PTSAR_RawScore)
 
-May22_F_PTSAR_TS <- merge(H1, Female_PTSAR_TS, by = "Raw_Score", all = FALSE )
-May22_F_PTSAR_TS <- May22_F_PTSAR_TS %>% rename(PTSAR_Raw_Score = Raw_Score,
+Dec21_F_PTSAR_TS <- merge(H1, Female_PTSAR_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_PTSAR_TS <- Dec21_F_PTSAR_TS %>% rename(PTSAR_Raw_Score = Raw_Score,
                                                 PTSAR_T = T,
                                                 PTSAR_Ptile  = Percentile)
 
@@ -124,10 +124,10 @@ May22_F_PTSAR_TS <- May22_F_PTSAR_TS %>% rename(PTSAR_Raw_Score = Raw_Score,
 ########################### PTS_TOT #########################
 Female_PTSTOT_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 9)
 
-I1 <- May22_F_RS %>% select(ID,May22_PTS_TOT_RawScore) %>% rename(Raw_Score = May22_PTS_TOT_RawScore)
+I1 <- Dec21_F_RS %>% select(ID,Dec21_PTS_TOT_RawScore) %>% rename(Raw_Score = Dec21_PTS_TOT_RawScore)
 
-May22_F_PTS_TOT_TS <- merge(I1, Female_PTSTOT_TS, by = "Raw_Score", all = FALSE )
-May22_F_PTS_TOT_TS <- May22_F_PTS_TOT_TS %>% rename(PTS_TOT_Raw_Score = Raw_Score,
+Dec21_F_PTS_TOT_TS <- merge(I1, Female_PTSTOT_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_PTS_TOT_TS <- Dec21_F_PTS_TOT_TS %>% rename(PTS_TOT_Raw_Score = Raw_Score,
                                                     PTS_TOT_T = T,
                                                     PTS_TOT_Ptile  = Percentile)
 
@@ -137,11 +137,11 @@ May22_F_PTS_TOT_TS <- May22_F_PTS_TOT_TS %>% rename(PTS_TOT_Raw_Score = Raw_Scor
 ########################### DIS #########################
 Female_DIS_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 10)
 
-J1 <- May22_F_RS %>% select(ID,May22_DIS_RawScore) %>% rename(Raw_Score = May22_DIS_RawScore)
+J1 <- Dec21_F_RS %>% select(ID,Dec21_DIS_RawScore) %>% rename(Raw_Score = Dec21_DIS_RawScore)
 
 
-May22_F_DIS_TS <- merge(J1, Female_DIS_TS, by = "Raw_Score", all = FALSE )
-May22_F_DIS_TS <- May22_F_DIS_TS%>% rename(DIS_Raw_Score = Raw_Score,
+Dec21_F_DIS_TS <- merge(J1, Female_DIS_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_DIS_TS <- Dec21_F_DIS_TS%>% rename(DIS_Raw_Score = Raw_Score,
                                            DIS_T = T,
                                            DIS_Ptile  = Percentile)
 #_________________________________________________________11
@@ -150,11 +150,11 @@ May22_F_DIS_TS <- May22_F_DIS_TS%>% rename(DIS_Raw_Score = Raw_Score,
 Female_SC_TS <- read_excel("Female_T_scores_v2.xlsx",sheet = 11)
 
 
-K1 <-  May22_F_RS %>% select(ID,May22_SC_RawScore) %>% rename(Raw_Score = May22_SC_RawScore)
+K1 <-  Dec21_F_RS %>% select(ID,Dec21_SC_RawScore) %>% rename(Raw_Score = Dec21_SC_RawScore)
 
 
-May22_F_SC_TS <- merge(K1, Female_SC_TS, by = "Raw_Score", all = FALSE )
-May22_F_SC_TS <- May22_F_SC_TS  %>% rename(SC_Raw_Score = Raw_Score,
+Dec21_F_SC_TS <- merge(K1, Female_SC_TS, by = "Raw_Score", all = FALSE )
+Dec21_F_SC_TS <- Dec21_F_SC_TS  %>% rename(SC_Raw_Score = Raw_Score,
                                            SC_T = T,
                                            SC_Ptile  = Percentile)
 
@@ -162,24 +162,24 @@ May22_F_SC_TS <- May22_F_SC_TS  %>% rename(SC_Raw_Score = Raw_Score,
 
 
 # Put all data frames into a list
-May22_F_list <- list(May22_F_Rl_TS,
-                     May22_F_ATR_TS,
-                     May22_F_ANX_TS,
-                     May22_F_DEP_TS,
-                     May22_F_ANG_TS,
-                     May22_F_PTSI_TS,
-                     May22_F_PTSAV_TS,
-                     May22_F_PTSAR_TS,
-                     May22_F_PTS_TOT_TS,
-                     May22_F_DIS_TS,
-                     May22_F_SC_TS)
+Dec21_F_list <- list(Dec21_F_Rl_TS,
+                     Dec21_F_ATR_TS,
+                     Dec21_F_ANX_TS,
+                     Dec21_F_DEP_TS,
+                     Dec21_F_ANG_TS,
+                     Dec21_F_PTSI_TS,
+                     Dec21_F_PTSAV_TS,
+                     Dec21_F_PTSAR_TS,
+                     Dec21_F_PTS_TOT_TS,
+                     Dec21_F_DIS_TS,
+                     Dec21_F_SC_TS)
 
 # Merge all data frames together
-May22_F_tidy <- May22_F_list %>% reduce(full_join, by='ID')
+Dec21_F_tidy <- Dec21_F_list %>% reduce(full_join, by='ID')
 
 # Print the resulting merged dataframe
-print(May22_F_tidy)
+print(Dec21_F_tidy)
 
 
-May22_F_tidy <- May22_F_tidy %>% select(ID, everything()) 
+Dec21_F_tidy <- Dec21_F_tidy %>% select(ID, everything()) 
 
--- RawScores2TScores.R
+++ RawScores2TScores.R
@@ -1,6 +1,7 @@
 # source('Male_RawScores2TScores.R')
 # source('Female_RawScores2TScores.R')
 
-May22_SCORES <- rbind(May22_M_tidy,May22_F_tidy)
+Dec21_SCORES <- arrange(rbind(Dec21_M_tidy,Dec21_F_tidy), ID)
 
-write.csv(May22_SCORES,file = "May22_Scores.csv",row.names = FALSE)
+
+write.csv(Dec21_SCORES,file = "Dec21_Scores.csv",row.names = FALSE)
