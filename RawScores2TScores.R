a55af63cd6d35f65cde325f8c3ce64520c7890ee
Alfieriek <alfieriek@gmail.com>
2024-03-30 13:40:09 -0500

    Added Demographics

--- RawScores2TScores.R
+++ RawScores2TScores.R
@@ -3,5 +3,8 @@
 
 Dec21_SCORES <- arrange(rbind(Dec21_M_tidy,Dec21_F_tidy), ID)
 
+Demographics <- read_excel("Demographics.xlsx")
+
+Dec21_SCORES <- merge(Demographics, Dec21_SCORES, by = "ID", all = FALSE )
 
 write.csv(Dec21_SCORES,file = "Dec21_Scores.csv",row.names = FALSE)
