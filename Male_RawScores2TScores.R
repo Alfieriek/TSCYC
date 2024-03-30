ed7d776033455288df6ae65f1e6fc213079fb388
Alfieriek <alfieriek@gmail.com>
2024-03-30 00:43:00 -0500

    For Jan21 Males Raw Scores have been changed to  t scores

--- Male_RawScores2TScores.R
+++ Male_RawScores2TScores.R
@@ -32,6 +32,8 @@ Male_ATR_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 2)
 B <- Jan21_M_RS %>% select(ID,Jan21_ATR_RawScore) %>% rename(Raw_Score = Jan21_ATR_RawScore)
 
 Jan21_M_ATR_TS <- merge(B, Male_ATR_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_ATR_TS <- Jan21_M_ATR_TS %>% select(-Raw_Score) %>% rename(ATR_T = T,
+                                                                   ATR_Ptile  = Percentile)
 
 
 
@@ -40,6 +42,11 @@ Jan21_M_ATR_TS <- merge(B, Male_ATR_TS, by = "Raw_Score", all = FALSE )
 ########################### ANX #########################
 Male_ANX_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 3)
 
+C <- Jan21_M_RS %>% select(ID,Jan21_ANX_RawScore) %>% rename(Raw_Score = Jan21_ANX_RawScore)
+
+Jan21_M_ANX_TS <- merge(C, Male_ANX_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_ANX_TS <- Jan21_M_ANX_TS %>% select(-Raw_Score) %>% rename(ANX_T = T,
+                                                                   ANX_Ptile  = Percentile)
 
 
 
@@ -48,7 +55,11 @@ Male_ANX_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 3)
 ########################### DEP #########################
 Male_DEP_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 4)
 
+D <- Jan21_M_RS %>% select(ID,Jan21_DEP_RawScore) %>% rename(Raw_Score = Jan21_DEP_RawScore)
 
+Jan21_M_DEP_TS <- merge(D, Male_DEP_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_DEP_TS <- Jan21_M_DEP_TS %>% select(-Raw_Score) %>% rename(DEP_T = T,
+                                                                   DEP_Ptile  = Percentile)
 
 
 #_________________________________________________________5
@@ -56,20 +67,35 @@ Male_DEP_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 4)
 ########################### ANG #########################
 Male_ANG_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 5)
 
+E <- Jan21_M_RS %>% select(ID,Jan21_ANG_RawScore) %>% rename(Raw_Score = Jan21_ANG_RawScore)
 
+Jan21_M_ANG_TS <- merge(E, Male_ANG_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_ANG_TS <- Jan21_M_ANG_TS %>% select(-Raw_Score) %>% rename(ANG_T = T,
+                                                                   ANG_Ptile  = Percentile)
 
 
 #_________________________________________________________6
 ########################### PTSI #########################
 Male_PTSI_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 6)
 
+f <- Jan21_M_RS %>% select(ID,Jan21_PTSI_RawScore) %>% rename(Raw_Score = Jan21_PTSI_RawScore)
 
+
+Jan21_M_PTSI_TS <- merge(f, Male_PTSI_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_PTSI_TS <- Jan21_M_PTSI_TS %>% select(-Raw_Score) %>% rename(PTSI_T = T,
+                                                                     PTSI_Ptile  = Percentile)
 
 
 #_________________________________________________________7
 ########################### PTSAV #########################
 Male_PTSAV_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 7)
 
+G <- Jan21_M_RS %>% select(ID,Jan21_PTSAV_RawScore) %>% rename(Raw_Score = Jan21_PTSAV_RawScore)
+
+
+Jan21_M_PTSAV_TS <- merge(G, Male_PTSAV_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_PTSAV_TS <- Jan21_M_PTSAV_TS %>% select(-Raw_Score) %>% rename(PTSAV_T = T,
+                                                                       PTSAV_Ptile  = Percentile)
 
 
 
@@ -77,6 +103,11 @@ Male_PTSAV_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 7)
 ########################### PTSAR #########################
 Male_PTSAR_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 8)
 
+H <- Jan21_M_RS %>% select(ID,Jan21_PTSAR_RawScore) %>% rename(Raw_Score = Jan21_PTSAR_RawScore)
+
+Jan21_M_PTSAV_TS <- merge(H, Male_PTSAR_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_PTSAV_TS <- Jan21_M_PTSAV_TS %>% select(-Raw_Score) %>% rename(PTSAV_T = T,
+                                                                       PTSAV_Ptile  = Percentile)
 
 
 
@@ -84,6 +115,11 @@ Male_PTSAR_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 8)
 ########################### PTS_TOT #########################
 Male_PTSTOT_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 9)
 
+I <- Jan21_M_RS %>% select(ID,Jan21_PTS_TOT_RawScore) %>% rename(Raw_Score = Jan21_PTS_TOT_RawScore)
+
+Jan21_M_PTS_TOT_TS <- merge(I, Male_PTSTOT_TS, by = "Raw_Score", all = FALSE )
+Jan21_M_PTS_TOT_TS <- Jan21_M_PTS_TOT_TS %>% select(-Raw_Score) %>% rename(PTS_TOT_T = T,
+                                                                           PTS_TOT_Ptile  = Percentile)
 
 
 
@@ -91,7 +127,7 @@ Male_PTSTOT_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 9)
 ########################### DIS #########################
 Male_DIS_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 10)
 
-
+J <- Jan21_M_RS %>% select(ID,Jan21_DIS_RawScore) %>% rename(Raw_Score = Jan21_DIS_RawScore)
 
 
 #_________________________________________________________11
@@ -100,4 +136,5 @@ Male_DIS_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 10)
 Male_SC_TS <- read_excel("Male_T_scores_v2.xlsx",sheet = 11)
 
 
+K <-  Jan21_M_RS %>% select(ID,Jan21_SC_RawScore) %>% rename(Raw_Score = Jan21_SC_RawScore)
 
