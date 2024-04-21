1be9b33c946e8990587107d3b5de9cc355e5c924
Alfieriek <alfieriek@gmail.com>
2024-04-21 01:08:23 -0500

    Performed Sign test for Scale t scores (Jan21 Vs May21) and (Dec21 Vs May22)

--- Pre_Post_Analysis.R
+++ Pre_Post_Analysis.R
@@ -13,7 +13,9 @@ May22_Data <- read.csv("May22_Scores.csv", header = T)
 str(Jan21_Data)
 
 T_Scores_Col <- c(grep("_T$", names(Jan21_Data), value = TRUE))
-
+Scales_name <- c("ResponseLevel", "AtypicalResponse", "Anxiety", "Depression", "Anger_Aggression",
+            "Posttraumatic_Stress_Intrusion", "Posttraumatic_Stress_Avoidance", "Posttraumatic_Stress_Arousal",
+            "Posttraumatic_Stress_Total","Dissociation", "SexualConcerns")
 
 # T_Scores_Col <- c(colnames(Jan21_Data[,1:5]),grep("_T$", names(Jan21_Data), value = TRUE))
 
@@ -54,4 +56,35 @@ for (i in c(1:11)) {
     select(contains(T_Scores_Col[i]))%>%
     gather(key = "Scale", value = "T_Score")
 }
+
+Jan_May_21_SignTest <- list()
+
+for (i in c(1:11)) {
+  Jan_May_21_SignTest[[i]] <- Jan_May_21_long[[i]] %>% wilcox_test (T_Score ~ Scale, paired = TRUE)
+
+}
+
+Jan_May_21_SignTest <- setNames(Jan_May_21_SignTest, Scales_name)
+
+
+Dec_May_22_long <- list()
+
+for (i in c(1:11)) {
+  Dec_May_22_long[[i]] <- cbind(Dec21_T_Scores,May22_T_Scores) %>% 
+    select(contains(T_Scores_Col[i]))%>%
+    gather(key = "Scale", value = "T_Score")
+}
+
+Dec_May_22_SignTest <- list()
+
+for (i in c(1:11)) {
+  Dec_May_22_SignTest[[i]] <- Dec_May_22_long[[i]] %>% wilcox_test (T_Score ~ Scale, paired = TRUE)
+  
+}
+
+Dec_May_22_SignTest <- setNames(Dec_May_22_SignTest, Scales_name)
+
+
+
+
 
